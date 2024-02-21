ac957fe58ad280410622e55390e52a9c9b313030
Sam <64200816+samihanine@users.noreply.github.com>
2024-02-20 19:27:31 -0500

    feat: add landing page

---
+++ apps/landing/app/(default)/page.tsx
@@ -0,0 +1,22 @@
+export const metadata = {
+  title: "Home - Simple",
+  description: "Page description",
+};
+
+import Hero from "@/components/hero-home";
+import Features from "@/components/features-home";
+import FeaturesBlocks from "@/components/features-blocks";
+import FeaturesWorld from "@/components/features-world";
+import Cta from "@/components/cta";
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <Features />
+      <FeaturesBlocks />
+      <FeaturesWorld />
+      <Cta />
+    </>
+  );
+}
---
+++ apps/landing/tailwind.config.js
@@ -0,0 +1,1 @@
+module.exports = require("@nexus/configs/tailwind/tailwind.config");
